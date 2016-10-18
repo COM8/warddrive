573261e039c1cb6d36df84b0ce806f833fe92820
COM8 <COM8@users.noreply.github.com>
2016-10-18 10:34:34 +0200

    space to tab

--- warddrive.py
+++ warddrive.py
@@ -24,14 +24,13 @@ def save():
         pickle.dump(loggedData, f, pickle.HIGHEST_PROTOCOL)
 		
 def setupGPIO():
-	GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(4, GPIO.IN)
     GPIO.setup(19, GPIO.OUT)
     GPIO.setup(13, GPIO.OUT)
     GPIO.setup(6, GPIO.OUT)
     GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.output(19, 1)
-    #GPIO.add_event_detect(23, GPIO.FALLING, callback=stop, bouncetime=200)
 
 def findOptimalInterface():
     best=0
@@ -50,15 +49,13 @@ def main():
     i = 0
     state = 0
     loggedData = None
-	
-	chdir("/home/pi/GPS/")
+    chdir("/home/pi/GPS/")
     try:
         with open("logFile" + '.pkl', 'rb') as f:
             loggedData = pickle.load(f)
     except FileNotFoundError:
         loggedData = dict()
-    
-	setupGPIO()
+    setupGPIO()
 	
     try:
         gpsd.connect()
@@ -70,7 +67,8 @@ def main():
         system("sudo gpsd /dev/ttyS0 -F /var/run/gpsd.sock")
         sleep(10)
         gpsd.connect()
-	interface='wlan' + str(findOptimalInterface())
+    interface='wlan' + str(findOptimalInterface())
+    print(interface)
     while True:
         try:
             if GPIO.input(23) == 0:
@@ -82,35 +80,35 @@ def main():
                     position = packet.position()
                     percision = packet.position_precision()
                     content = iwlist.scan(interface='wlan' + str(interface))
-                        if len(content) > 0:
-                            for element in iwlist.parse(content):
-                                element["position"] = position
-                                element["percision"] = percision
-                                del element["cellnumber"]
-                                try:
-                                    dif1 = (percision[0] + percision[1])
-                                    dif2 = loggedData[element["mac"]]["percision"][
-                                        0] + loggedData[element["mac"]]["percision"][1]
-                                    if dif1 < dif2:
-                                        if dif1 + 10 <= dif2:
-                                            loggedData[element["mac"]][
-                                                "position"] = position
-                                            loggedData[element["mac"]][
-                                                "percision"] = percision
-                                        else:
-                                            loggedData[element["mac"]]["position"] = (((loggedData[element["mac"]]["position"][
-                                                                                      0]) + element["position"][0]) / 2, ((loggedData[element["mac"]]["position"][1]) + element["position"][1]) / 2)
-                                            loggedData[element["mac"]]["percision"] = ((percision[0] + loggedData[element["mac"]][
-                                                                                       "percision"][0]) / 2, (percision[1] + loggedData[element["mac"]]["percision"][1]) / 2)
-                                    elif dif1 >= dif2:
-                                        if dif1 <= (dif2 + 10):
-                                            loggedData[element["mac"]]["position"] = (((loggedData[element["mac"]]["position"][
-                                                                                      0]) + element["position"][0]) / 2, ((loggedData[element["mac"]]["position"][1]) + element["position"][1]) / 2)
-                                            loggedData[element["mac"]]["percision"] = ((percision[0] + loggedData[element["mac"]][
-                                                                                       "percision"][0]) / 2, (percision[1] + loggedData[element["mac"]]["percision"][1]) / 2)
-                                except KeyError:
-                                    print(element)
-                                    loggedData[element["mac"]] = element
+                    if len(content) > 0:
+                        for element in iwlist.parse(content):
+                            element["position"] = position
+                            element["percision"] = percision
+                            del element["cellnumber"]
+                            try:
+                                dif1 = (percision[0] + percision[1])
+                                dif2 = loggedData[element["mac"]]["percision"][
+                                    0] + loggedData[element["mac"]]["percision"][1]
+                                if dif1 < dif2:
+                                    if dif1 + 10 <= dif2:
+                                        loggedData[element["mac"]][
+                                            "position"] = position
+                                        loggedData[element["mac"]][
+                                            "percision"] = percision
+                                    else:
+                                        loggedData[element["mac"]]["position"] = (((loggedData[element["mac"]]["position"][
+                                                                                  0]) + element["position"][0]) / 2, ((loggedData[element["mac"]]["position"][1]) + element["position"][1]) / 2)
+                                        loggedData[element["mac"]]["percision"] = ((percision[0] + loggedData[element["mac"]][
+                                                                                   "percision"][0]) / 2, (percision[1] + loggedData[element["mac"]]["percision"][1]) / 2)
+                                elif dif1 >= dif2:
+                                    if dif1 <= (dif2 + 10):
+                                        loggedData[element["mac"]]["position"] = (((loggedData[element["mac"]]["position"][
+                                                                                  0]) + element["position"][0]) / 2, ((loggedData[element["mac"]]["position"][1]) + element["position"][1]) / 2)
+                                        loggedData[element["mac"]]["percision"] = ((percision[0] + loggedData[element["mac"]][
+                                                                                   "percision"][0]) / 2, (percision[1] + loggedData[element["mac"]]["percision"][1]) / 2)
+                            except KeyError:
+                                print(element)
+                                loggedData[element["mac"]] = element
                         else:
                             break
                 except gpsd.NoFixError:
